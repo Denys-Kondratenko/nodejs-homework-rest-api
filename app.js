b2f94b99175219c9acfab5a79b1542bdc70a2a73
Denys-Kondratenko <dk132652@gmail.com>
2025-02-10 12:19:37 +0200

    1

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+// require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const app = express()
+// const mongoose = require("mongoose");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.DB_URI, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// const { MongoClient } = require("mongodb");
 
-app.use('/api/contacts', contactsRouter)
+// const DB_URI = process.env.DB_URI;
+
+// console.log(DB_URI);
+
+// const client = new MongoClient(DB_URI);
+
+// async function run() {
+//   try {
+//     await client.connect();
+
+//     await client.db("admin").command({ ping: 1 });
+//     console.info(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// run().catch(console.dir);
+
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
+
+// denys
+// 2b9nIsRwBDRcxkNa
